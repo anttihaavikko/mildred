57fda41f9bbd1d018a54fb85b116de29e380d338
Antti Haavikko <antti.haavikko@focus.fi>
2018-02-24 19:26:26 +0200

    Add clouds

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	private float speed;
+
+	private float min = -150f;
+	private float max = 150f;
+
+	// Use this for initialization
+	void Awake () {
+
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+
+		float r = Random.value * 0.005f;
+
+		float depth = 2 + Random.Range(-1f, 7f);
+
+		float xdir = (Random.value < 0.5f) ? 1f : -1f;
+		float ydir = 1f;
+
+		transform.localPosition = new Vector3 (transform.localPosition.x + Random.Range(-20, 20), transform.localPosition.y + Random.Range(-5, 5), depth);
+		transform.localScale = new Vector3 (xdir * (1f + r), ydir * (1f + r), 1f);
+
+		sprite.color = new Color (1, 1, 1, 0.1f + Random.value / 10f);
+
+		speed = 0.1f + Random.value * 0.5f;
+	}
+
+	void Update() {
+		transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+		if (transform.localPosition.x > max) {
+			transform.localPosition = new Vector3 (min, transform.localPosition.y, transform.localPosition.z);
+		}
+	}
+}
--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -19,12 +19,16 @@ public class FollowCamera : MonoBehaviour {
 	void Update () 
 	{
 		if (target) {
-			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+			Vector3 point = Camera.main.WorldToScreenPoint(target.position);
 			Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
 			Vector3 destination = transform.position + delta + new Vector3(offset.x * target.localScale.x, offset.y, offset.z);
 			float z = Mathf.MoveTowards (transform.position.z, -6f - body.velocity.magnitude * 0.75f, Time.deltaTime * 10f);
 			destination.z = z;
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+			Quaternion targetRot = Quaternion.Euler(new Vector3 (0, Mathf.Clamp(body.angularVelocity * -0.005f, -5f, 5f), 0f));
+//			transform.rotation = targetRot;
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRot, Time.deltaTime * 2f);
 		}
 
 	}
