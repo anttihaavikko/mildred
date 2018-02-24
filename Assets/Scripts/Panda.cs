84e44e06467c94fd152745a3f044f2a414b1202b
Antti Haavikko <antti.haavikko@focus.fi>
2018-02-24 12:34:46 +0200

    Add collectibles

--- Assets/Scripts/Panda.cs
+++ Assets/Scripts/Panda.cs
@@ -18,32 +18,51 @@ public class Panda : MonoBehaviour {
 	private bool grounded = false;
 	public LayerMask groundLayer;
 
+	private Vector3 spawnPoint = Vector3.zero;
+
+	private int points = 0;
+
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
+		spawnPoint = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.localScale = Vector3.MoveTowards(transform.localScale, targetSize, Time.deltaTime);
 
-		grounded = Physics2D.Raycast (transform.position, Vector2.down, 1f, groundLayer);
-		Color c = grounded ? Color.green : Color.red;
-		Debug.DrawRay (transform.position, Vector2.down, c);
+		grounded = false;
+
+		for (int i = -1; i < 2; i++) {
+			Vector3 spot = transform.position + i * Vector3.left * 0.5f;
+			bool spotGrounded = Physics2D.Raycast (spot, Vector3.down, 1f, groundLayer);
+
+			Color c = spotGrounded ? Color.green : Color.red;
+			Debug.DrawRay (spot, Vector3.down, c);
+
+			if (spotGrounded) {
+				grounded = true;
+				break;
+			}
+		}
+			
 	}
 
 	void FixedUpdate() {
 		float dir = InputMagic.Instance.GetAxis (InputMagic.STICK_OR_DPAD_X);
 		body.AddTorque (-dir * rollForce);
+
 		targetSize = (InputMagic.Instance.GetButton (InputMagic.A)) ? new Vector3 (1.1f, 0.9f, 1f) : new Vector3 (1f, 1f, 1f);
 
 		if(Physics2D.OverlapCircle(transform.position, 0.2f, groundLayer)) {
-			Reset();
+			Die();
 		}
 	}
 
 	void LateUpdate() {
-		if (grounded && InputMagic.Instance.GetButtonUp (InputMagic.A)) {
+		
+		if (grounded && InputMagic.Instance.GetButtonDown (InputMagic.A)) {
 			body.velocity = new Vector2 (body.velocity.x, 0);
 			body.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
 		}
@@ -51,7 +70,7 @@ public class Panda : MonoBehaviour {
 		body.angularVelocity = Mathf.Clamp (body.angularVelocity, -maxRotationSpeed, maxRotationSpeed);
 
 		if (Application.isEditor && Input.GetKeyDown (KeyCode.R)) {
-			Reset ();
+			Die ();
 		}
 
 		int rollDir = 0;
@@ -73,18 +92,42 @@ public class Panda : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.relativeVelocity.magnitude > 50f) {
-			Reset ();
+			Die ();
+		}
+
+		if (coll.gameObject.tag == "Spikes") {
+			Die ();
 		}
 	}
 
-	void Reset() {
+	void OnTriggerEnter2D(Collider2D coll) {
+		
+		if (coll.tag == "Checkpoint") {
+			spawnPoint = coll.transform.position;
+		}
+
+		if (coll.tag == "Leaf") {
+			Destroy (coll.gameObject);
+			points++;
+			Debug.Log (points + " points");
+		}
+	}
+
+	void Die() {
+		gameObject.SetActive (false);
+		Invoke ("Respawn", 2f);
+	}
+
+	void Respawn() {
 		body.velocity = Vector2.zero;
 		body.angularVelocity = 0f;
 
 		tailBody.velocity = Vector2.zero;
 		tailBody.angularVelocity = 0f;
 
-		transform.position = Vector3.zero;
+		transform.position = spawnPoint;
 		tailBody.transform.localPosition = new Vector3 (0, -1.783f, 0f);
+
+		gameObject.SetActive (true);
 	}
 }
